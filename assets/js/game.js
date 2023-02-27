a403dce55d701c3c9f8539c7884ad6a3372aafc9
evanbruce38 <106777653+evanbruce38@users.noreply.github.com>
2023-02-27 18:08:32 -0500

    added skip fight functionality

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,7 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
 console.log(playerName, playerAttack, playerHealth);
 
@@ -10,7 +11,10 @@ var enemyAttack = 12;
 
 var fight = function () {
     window.alert("Welcome to Robot Gladiators!");
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
+    // if player chooses to fight, then fight 
+    if (promptFight === "fight" || promptFight === "FIGHT") { 
     // player attacks
     enemyHealth = enemyHealth - playerAttack;
     console.log(
@@ -36,6 +40,21 @@ var fight = function () {
     } else {
         window.alert(playerName + " still has " + playerHealth + " health left.");
     }
+
+    // if player chooses to skip or typo
+} else if (promptFight === "skip" || promptFight === "SKIP") {
+    // confirm player wants to skip
+    var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+    //if yes (true) leave fight
+    if (confirmSkip) {
+        window.alert(playerName + " has decided to skip this fight. Goodbye!");
+        playerMoney = playerMoney - 2;
+    }
+
+} else {
+    window.alert("You need to choose a valid option. Try again!");
+}
 };
 
 fight();
